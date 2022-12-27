33d90e695aec19654a32af7c3692330ea63b2bed
Tewodrosyeteji <teddytejinesh@gmail.com>
2022-12-27 12:38:00 +0300

    sample Modal

---
+++ Modal/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+const showModal=document.querySelectorAll('.show-modal');
+const hiddenModal=document.querySelector('.modal');
+const colseModal=document.querySelector('.close-modal');
+const overlay=document.querySelector('.overlay');
+
+
+const open=function(){
+    hiddenModal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const close=function(){
+    hiddenModal.classList.add('hidden');
+    overlay.classList.add('hidden')}
+
+for(let i=0;i<showModal.length;i++){
+    showModal[i].addEventListener('click',open );
+};
+
+colseModal.addEventListener('click',close);
+overlay.addEventListener('click',close);
+
+document.addEventListener('keydown',function(e){
+console.log(e.key);
+if(e.key === 'Escape' && (!hiddenModal.classList.contains('hidden'))){
+   close();
+}
+});
+
+
+
+
