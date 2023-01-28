2aa986226732cd254ee6eaa16b9092199a5ee2e9
Tewodrosyeteji <teddytejinesh@gmail.com>
2023-01-28 09:36:01 +0300

    oop done

--- OOP/script.js
+++ OOP/script.js
@@ -65,41 +65,85 @@ console.log(ford.speedUS);
 ford.speedUS=50;
 console.log(ford);
 
-//challenge three
+//challenge four
 
 
 
-const Car =function(make,speed){
+class CarCl {
+    constructor(make,speed){
     this.make=make;
     this.speed=speed;
     }
-    
-  
-    
-    Car.prototype.brake=function(){
+
+
+    brake(){
         this.speed -=5;
         console.log(this.speed);
+        return this;
     }
-    
-
-
-    const EV =function(make,speed,charge){
-        Car.call(this,make,speed);
-        this.charge=charge;
+    }
+    class EVCL extends CarCl{
+        #charge;
+        constructor(make,speed,charge){
+            super(make,speed);
+             this.#charge=charge;
         }
-        
-        
-        EV.prototype=Object.create(Car.prototype);
-        
-        EV.prototype.chargeBattry=function(chargeTo){
-            this.charge=chargeTo;
+
+        chargeBattry(chargeTo){
+            this.#charge=chargeTo;
         }
-        
-        EV.prototype.accelerate=function(){
+
+        accelerate(){
             this.speed += 20;
-            this.charge -= 0.01;
-            console.log(`Tesla going at ${this.speed} km/h with a charge of ${this.charge * 100}%` );
+            this.#charge -= 0.01;
+            console.log(`Tesla going at ${this.speed} km/h with a charge of ${this.#charge * 100}%` );
+            return this;
         }
-        const tesla=new EV('Tesla',120,0.23)
+        
+        }
 
-        tesla.accelerate();
+        
+        const rivian=new EVCL('Rivian',120,0.23)
+
+        rivian.accelerate();
+        rivian.brake();
+        rivian.accelerate().accelerate().brake();
+
+
+
+        //challenge three
+
+        const Car =function(make,speed){
+            this.make=make;
+            this.speed=speed;
+            }
+            
+          
+            
+            Car.prototype.brake=function(){
+                this.speed -=5;
+                console.log(this.speed);
+            }
+            
+        
+        
+            const EV =function(make,speed,charge){
+                Car.call(this,make,speed);
+                this.charge=charge;
+                }
+                
+                
+                EV.prototype=Object.create(Car.prototype);
+                
+                EV.prototype.chargeBattry=function(chargeTo){
+                    this.charge=chargeTo;
+                }
+                
+                EV.prototype.accelerate=function(){
+                    this.speed += 20;
+                    this.charge -= 0.01;
+                    console.log(`Tesla going at ${this.speed} km/h with a charge of ${this.charge * 100}%` );
+                }
+                const tesla=new EV('Tesla',120,0.23)
+        
+                tesla.accelerate();
